23f570e03aae1586ff78bfae3397f6e579fea83e
M-BilalArif <bilalarif0529@gmail.com>
2025-07-25 00:01:37 +0500

    Add files via upload

---
+++ ZFX Contract.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ZFurgeX_Token is ERC20, Ownable { 
+    uint256 public  constant maxSupply  = 500000000*1e18;
+    constructor() ERC20("ZFurgeX", "ZFX") Ownable(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4) {  
+     }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        require (amount+totalSupply() <= maxSupply, "Max Supply reached");
+        _mint(to, amount);
+    }
+
+    function transferFromContract(address _to,uint256 amount) external onlyOwner {
+        require(amount > 0 , "Amount must be greater than 0");
+        _transfer(address(this),_to,amount);
+    }
+
+    function burn( uint256 amount) public   {
+        _burn(msg.sender, amount);
+    }
+}
